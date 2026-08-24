d6adaaffb59d08b74afd098ff3addca421eab5df
Baraiya Kishan <taskmaster4747@gmail.com>
2026-08-25 00:23:22 +0530

    Game Update

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+
+    public int level;
+
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+
+        if (PlayerPrefs.GetInt("LevelReached") < level)
+        {
+            btn.interactable = false;
+        }
+    } 
+}
--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class NextLevel : MonoBehaviour
 {
     public string nextLevelName;
+    public int nextLevelValue;
 
     public void LoadNextLevel()
     {
+        PlayerPrefs.SetInt("LevelReached", nextLevelValue);
         UnityEngine.SceneManagement.SceneManager.LoadScene(nextLevelName);
         Time.timeScale = 1;
     }
